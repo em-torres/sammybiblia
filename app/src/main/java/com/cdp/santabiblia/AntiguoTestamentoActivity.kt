86b52185e438990aa616f163b076922a091bd025
Edwin Marte Torres <kdgpro15@gmail.com>
2022-07-25 13:05:43 -0400

    Arreglando el Activity Principal

--- app/src/main/java/com/cdp/santabiblia/AntiguoTestamentoActivity.kt
+++ app/src/main/java/com/cdp/santabiblia/AntiguoTestamentoActivity.kt
@@ -1,11 +1,15 @@
 package com.cdp.santabiblia
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity53 : AppCompatActivity() {
+class AntiguoTestamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main53)
+        setContentView(R.layout.activity_main2)
     }
 }
+
+
--- app/src/main/java/com/cdp/santabiblia/MainActivity.kt
+++ app/src/main/java/com/cdp/santabiblia/MainActivity.kt
@@ -3,7 +3,6 @@ package com.cdp.santabiblia
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat.startActivity
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -11,20 +10,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initEvents()
+        val button = findViewById<Button>(R.id.btn_antiguo_testamento)
+        button.setOnClickListener() {
+            val intent_antiguo_testamento = Intent(this, AntiguoTestamentoActivity::class.java)
+            startActivity(intent_antiguo_testamento)
+        }
 
+        initEvents()
     }
 
     private fun initEvents() {
-        val button: Bu= findViewById<Button>(R.id.button)
-        val button: Button = findViewById<Button>(R.id.button2)
-        val button: Button = findViewById<Button>(R.id.button3)
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            val intent = Intent(this, MainActivity3::class.java)
-            val intent = Intent(this, MainActivity4::class.java)
-            startActivity(intent)
-        }
+//        val button: Button findViewById<>
+//        val button: Button = findViewById<Button>(R.id.button2)
+//        val button: Button = findViewById<Button>(R.id.button3)
+//        button.setOnClickListener {
+//            val intent = Intent(this, MainActivity2::class.java)
+//            val intent = Intent(this, MainActivity3::class.java)
+//            val intent = Intent(this, MainActivity4::class.java)
+//            startActivity(intent)
     }
+}
 
 
